d84e233110747b9be7180984656041a24a1a7c35
Mehmet Çağrı Ekici <101545596+mehmetcagriekici@users.noreply.github.com>
2024-05-06 12:17:15 +0300

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabaseURL = 'https://tocmsjmuyjfxsxpwjznd.supabase.co'
+export const supabaseURL = 'your supabase url'
 export const key =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRvY21zam11eWpmeHN4cHdqem5kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE1MjQ3MDYsImV4cCI6MjAyNzEwMDcwNn0.O8tW2uHYDrXU-Gj693dUni1DMkh9kXishw1joftkxGY'
+  'your supabase key'
 
 ///supabase url + anon key
 const supabase = createClient(supabaseURL, key)
